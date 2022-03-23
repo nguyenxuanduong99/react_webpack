1c0192b2bc4a3782f3ef93259b1d6160edadcb65
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2022-03-23 17:16:47 +0700

    import thu vien css

--- webpack.config.js
+++ webpack.config.js
@@ -41,15 +41,20 @@ module.exports = {
                     },
                 ]
             },
+            // {
+            //     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            //     use: [{
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: '[name].[ext]',
+            //             outputPath: 'fonts/'
+            //         }
+            //     }]
+            // },
             {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
+                test: /\.(woff(2)?|ttf|eot|svg)$/i,
+                type: 'asset/resource',
+                dependency: {not: ['url']},
             },
         ]
     },
